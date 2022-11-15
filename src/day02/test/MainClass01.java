4febf4a11f9dc6a095cf0f47c50121d8b8b8fbcc
김수로헌 <@DESKTOP-OBGDBEG>
2022-11-16 02:54:53 +0900

    첫 커밋

---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,21 @@
+package day02.test;
+/*
+ * 주석이란?
+ *   컴퓨터가 코드로 인식하지 않는다.
+ *  
+ */
+
+/*
+ * 여러줄 주석
+ * 
+ */
+
+// 한줄주석
+public class MainClass01 {
+	//프로그램 시작이 되는 메인메소드
+	public static void main(String[] args) {
+		System.out.println("메인메소드 시작!");
+		
+		
+	}
+}
---
+++ src/생활코딩/자바연습.java
@@ -0,0 +1,8 @@
+package 생활코딩;
+
+public class 자바연습 {
+	
+	public static void main(String[] args) {
+		System.out.println("이클립스 안녕");
+	}
+}
